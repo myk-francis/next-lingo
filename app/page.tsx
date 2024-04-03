7d367c06ec98886ec0a8eae53309430425e7f9eb
myk <michaelmugendi1707@gmail.com>
2024-04-03 10:04:00 +0300

    Update: Primary button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col max-w-[200px] p-4 space-y-4 ">
+      <Button className="">Default</Button>
+      <Button className="">Primary</Button>
       <Button className="">Click Me</Button>
     </div>
   );
