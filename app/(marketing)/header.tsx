599da71264e2e3c7a1d5f9f40ee980adfcebd5b0
myk <michaelmugendi1707@gmail.com>
2024-04-03 11:54:29 +0300

    Add: mascot

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      <h1 className="text-4xl font-bold">Header</h1>
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src={"/mascot.svg"} width={40} height={40} alt={"Mascot"} />
+          <h1 className="text-2xl font-extrabold to-green-600 trancking-wide">
+            Lingo
+          </h1>
+        </div>
+      </div>
     </header>
   );
 };
