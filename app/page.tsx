1b53b76aebbe11ea8f787587e4a29f4f6658628d
myk <michaelmugendi1707@gmail.com>
2024-04-03 09:56:47 +0300

    Update: changed button signature

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button>Click Me</Button>
+      <Button className="">Click Me</Button>
     </div>
   );
 }
