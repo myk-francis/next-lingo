466fdc7cf8c607d15fdacb059b34316193a081e1
myk <michaelmugendi1707@gmail.com>
2024-04-04 12:03:59 +0300

    Update: mobile-header

---
+++ components/mobile-header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MobileHeader = () => {
+  return (
+    <div className="lg:hidden px-6 h-[50px] flex items-center bg-yellow-500 border-b top-0 w-full z-50">
+      MobileHeader
+    </div>
+  );
+};
