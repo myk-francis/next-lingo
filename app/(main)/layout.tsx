8085f612cade63ca8bf4b5e5998ab6738de466c0
myk <michaelmugendi1707@gmail.com>
2024-04-04 11:56:22 +0300

    Update: sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,17 @@
+import { Sidebar } from "@/components/sidebar";
+
 type Props = {
   children: React.ReactNode;
 };
 
 const MainLayout = ({ children }: Props) => {
   return (
-    <main className="pl-[256px] h-full">
-      <div className="bg-red-500 h-full">{children}</div>
-    </main>
+    <>
+      <Sidebar className="hidden lg:flex" />
+      <main className="pl-[256px] h-full">
+        <div className="bg-red-500 h-full">{children}</div>
+      </main>
+    </>
   );
 };
 
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,18 @@
-export const Sidebar = () => {
-  return <div>sidebar</div>;
+import { cn } from "@/lib/utils";
+
+type Props = {
+  className?: string;
+};
+
+export const Sidebar = ({ className }: Props) => {
+  return (
+    <div
+      className={cn(
+        " bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
+      )}
+    >
+      sidebar
+    </div>
+  );
 };
