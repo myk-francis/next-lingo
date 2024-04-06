28408206ffd4fa7e1874c352c32bac68762f6093
myk <michaelmugendi1707@gmail.com>
2024-04-06 10:52:45 +0300

    Update: SidebarItem

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
 
 type Props = {
   className?: string;
@@ -13,11 +15,16 @@ export const Sidebar = ({ className }: Props) => {
         className
       )}
     >
-      <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-        <Image src={"/mascot.svg"} width={40} height={40} alt={"Mascot"} />
-        <h1 className="text-2xl font-extrabold to-green-600 trancking-wide">
-          Lingo
-        </h1>
+      <Link href={"/learn"}>
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src={"/mascot.svg"} width={40} height={40} alt={"Mascot"} />
+          <h1 className="text-2xl font-extrabold to-green-600 trancking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem />
       </div>
     </div>
   );
