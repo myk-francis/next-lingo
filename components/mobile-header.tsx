6e307181777fcfb8f9b4995b0a1ee5b9e0002130
myk <michaelmugendi1707@gmail.com>
2024-04-06 09:10:01 +0300

    Update: finished Mobile hearder

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -3,7 +3,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export const MobileHeader = () => {
   return (
-    <div className="lg:hidden px-6 h-[50px] flex items-center bg-yellow-500 border-b top-0 w-full z-50">
+    <div className="lg:hidden px-6 h-[50px] flex items-center bg-green-500 border-b top-0 w-full z-50">
       <MobileSidebar />
     </div>
   );
