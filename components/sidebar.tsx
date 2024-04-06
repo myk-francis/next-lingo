a62b24d08eb96e8fcd6bba3d08ca664e4c545b6d
myk <michaelmugendi1707@gmail.com>
2024-04-06 09:15:37 +0300

    Update: added header to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 type Props = {
   className?: string;
@@ -8,11 +9,16 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        " bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      sidebar
+      <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+        <Image src={"/mascot.svg"} width={40} height={40} alt={"Mascot"} />
+        <h1 className="text-2xl font-extrabold to-green-600 trancking-wide">
+          Lingo
+        </h1>
+      </div>
     </div>
   );
 };
