2e5393a09f833c92fd8fdacc202af2b0cc98afe1
myk <michaelmugendi1707@gmail.com>
2024-04-06 11:02:10 +0300

    Update: SidebarItem butons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -24,7 +24,7 @@ export const Sidebar = ({ className }: Props) => {
         </div>
       </Link>
       <div className="flex flex-col gap-y-2 flex-1">
-        <SidebarItem />
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
       </div>
     </div>
   );
