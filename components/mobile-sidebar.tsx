05940b542b6d875625a60eb591e9a0139356e6e8
myk <michaelmugendi1707@gmail.com>
2024-04-06 09:02:08 +0300

    Update: Mobileheader

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+import { Sidebar } from "./sidebar";
 
 export const MobileSidebar = () => {
-  return <div>mobile-sidebar</div>;
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-white" />
+      </SheetTrigger>
+      <SheetContent>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
 };
