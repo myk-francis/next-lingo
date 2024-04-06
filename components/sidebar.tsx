389f2cf6441046ca087fed801e64455ae0a4ffb4
myk <michaelmugendi1707@gmail.com>
2024-04-06 11:19:44 +0300

    Update: fixed flex bug

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { SidebarItem } from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -11,7 +13,7 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex flex-col",
         className
       )}
     >
@@ -23,8 +25,25 @@ export const Sidebar = ({ className }: Props) => {
           </h1>
         </div>
       </Link>
+
       <div className="flex flex-col gap-y-2 flex-1">
         <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Learderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
+        <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+      </div>
+
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
       </div>
     </div>
   );
