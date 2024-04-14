8efcad0edb5413f8db43eaf3fb19f0666ec0dd51
myk <michaelmugendi1707@gmail.com>
2024-04-14 10:47:07 +0300

    Update: sticky wrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,12 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
 import React from "react";
 
 const LearnPage = () => {
-  return <div>LearnPage</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>LearnPage</StickyWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
