9e79f0359853704e1108117c710c6e8666695410
myk <michaelmugendi1707@gmail.com>
2024-05-02 11:50:11 +0300

    Update: card implemented

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+import { Check } from "lucide-react";
 import React from "react";
 
 type Props = {
@@ -17,5 +19,21 @@ export const Card = ({
   disabled = false,
   active = false,
 }: Props) => {
-  return <div>Card</div>;
+  return (
+    <div
+      onClick={() => onClick(id)}
+      className={cn(
+        "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]",
+        disabled && "opacity-50 pointer-events-none"
+      )}
+    >
+      <div className="min-h-[24px] w-full flex items-center justify-end">
+        {active && (
+          <div className="rounded-md bg-green-600 flex items-center justify-center p-1.5">
+            <Check className="text-white stroke-[4] h-4 w-4" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
