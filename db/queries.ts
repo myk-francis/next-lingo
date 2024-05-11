b462f6848ac856f506b72d40ae1332dc330c1eee
myk <michaelmugendi1707@gmail.com>
2024-05-11 11:03:31 +0300

    Update: getUserProgress

--- db/queries.ts
+++ db/queries.ts
@@ -3,9 +3,27 @@ import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs";
 
 import db from "@/db/drizzle";
+import { userProgress } from "./schema";
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
 
   return data;
 });
